e97ff1f5300b7754c89b9ca21b5763fa38dc7138
smartgatekeeperai <smartdermaph@gmail.com>
2025-11-30 02:19:06 +0800

    updates

--- controller.js
+++ controller.js
@@ -51,7 +51,7 @@ export function createControllers({ pool }) {
         throw new Error('No active API keys found in dbo."APIKeyManagement"');
       }
 
-      const row = selectRes.camelcaseKeys(rows[0]);
+      const row = rows[0];
 
       await client.query(
         `
